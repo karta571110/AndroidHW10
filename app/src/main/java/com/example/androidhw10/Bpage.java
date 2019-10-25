d330b508d79ec7f1bbb9955f0ce38955b6685577
karta571110 <44608844+karta571110@users.noreply.github.com>
2019-10-25 16:32:02 +0800

    Apage OK

--- app/src/main/java/com/example/androidhw10/Bpage.java
+++ app/src/main/java/com/example/androidhw10/Bpage.java
@@ -2,13 +2,19 @@ package com.example.androidhw10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Bpage extends AppCompatActivity {
-
+    int ApageChoise;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bpage);
+
+        int ApageChoose;
+        Intent Adata=getIntent();
+        ApageChoose=Adata.getIntExtra("month",0);
+
     }
 }
--- app/src/main/java/com/example/androidhw10/MainActivity.java
+++ app/src/main/java/com/example/androidhw10/MainActivity.java
@@ -2,13 +2,74 @@ package com.example.androidhw10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+int Month;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button onetwobtn = (Button) findViewById(R.id.onetwobtn);
+        Button threefourbtn = (Button) findViewById(R.id.threefourbtn);
+        Button fivesixbtn = (Button) findViewById(R.id.fivesixbtn);
+        Button seveneightbtn = (Button) findViewById(R.id.seveneightbtn);
+        Button ninetenbtn = (Button) findViewById(R.id.ninetenbtn);
+        Button eleventwelvebtn = (Button) findViewById(R.id.eleventwelvebtn);
+
+
+
+        onetwobtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Month=1;
+               sendtob();
+            }
+        });
+        threefourbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Month=3;
+                sendtob();
+            }
+        });
+        fivesixbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Month=5;
+                sendtob();
+            }
+        });
+        seveneightbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Month=7;
+                sendtob();
+            }
+        });
+        ninetenbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Month=9;
+                sendtob();
+            }
+        });
+        eleventwelvebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Month=11;
+                sendtob();
+            }
+        });
+
+    }
+    public  void sendtob(){
+        Intent sendatob=new Intent(MainActivity.this,Bpage.class);
+        sendatob.putExtra("month",Month);
+        startActivity(sendatob);
+
     }
 }
